c371633c29f2aa442c0687922b9f493cb058c074
t3chnique <144436602+t3chnique@users.noreply.github.com>
2023-12-15 15:40:43 +0200

    photo + text
    and some other changes

--- main_telegram.py
+++ main_telegram.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
 import uuid
 import requests
 import pywhatkit as kt
@@ -7,6 +7,7 @@ import telebot
 import time
 # from telebot import types
 from dotenv import load_dotenv
+from art import text2art
 load_dotenv()
 token = os.getenv('TOKEN')
 me = os.getenv('ME')
@@ -14,6 +15,8 @@ bot = telebot.TeleBot(token)
 
 media_folder = 'media'
 os.makedirs(media_folder, exist_ok=True)
+
+random_filename = str(uuid.uuid4())
 
 
 def timer(message):
@@ -24,20 +27,24 @@ def timer(message):
 def delete_message_message(message):
     for i in range(0, 1):
         bot.delete_message(message.chat.id, message.message_id-i)
-# chat_id = 6499881879
 
 
 @bot.message_handler(commands=["start"])
 def check_system(message):
     user_id = message.from_user.id
-    bot.send_message(user_id, '```\nprint("hello! Send me a photo")\n```',
+    bot.send_message(user_id, '```\nprint("Hello! Send me a photo")\n```',
                      parse_mode='MarkdownV2')
+    image_hint = "nori.png"
+    response = '```\n# hint\n```' + '```\nprint("It is better to upload simple photos:")\n```'
+    file_hint = open('./' + image_hint, 'rb')
+    bot.send_photo(message.chat.id, file_hint, caption=response, parse_mode='MarkdownV2')
     wait_for_photo(message)
+    delete_message_message(message)
 
 
+@bot.message_handler(commands=["photo"])
 def wait_for_photo(message):
     user_id = message.from_user.id
-    delete_message_message(message)
     if message and message.photo:
         # Get the file ID of the latest photo
         file_id = message.photo[-1].file_id
@@ -51,9 +58,6 @@ def wait_for_photo(message):
         # Download the photo
         response = requests.get(file_url)
 
-        # Save the photo locally
-        random_filename = str(uuid.uuid4())
-
         photo_path = f"{media_folder}/{user_id}_{random_filename}.jpg"
         with open(photo_path, 'wb') as f:
             f.write(response.content)
@@ -65,8 +69,6 @@ def wait_for_photo(message):
         output_file_path_1 = f"{media_folder}/ascii_{random_filename}"
         kt.image_to_ascii_art(photo_path, output_file_path_1)
 
-        # with open(output_file_path + '.txt', 'rb') as f:
-        # bot.send_document(user_id, f)
         with open(output_file_path_1 + '.txt', "r") as file:
             output_file_path_1 = file.read()
         text_content = f"""
@@ -74,17 +76,19 @@ def wait_for_photo(message):
         Made by < leo >.
         """
         output_image_path_2 = f"{media_folder}/output_{random_filename}.png"
-        text_to_image(text_content, output_image_path_2)
+        text_to_image(text_content, output_image_path_2, contrast_factor=1.5)
         image = output_image_path_2
         file = open('./' + image, 'rb')
         bot.send_photo(message.chat.id, file)
+        bot.send_message(user_id, '```\nprint("done, can send another!")\n```',
+                         parse_mode='MarkdownV2')
     else:
         time.sleep(5)
         # bot.reply_to(message, "No photo found in the message."
         # "Please send a photo.")
 
 
-def text_to_image(input_text, output_image_path):
+def text_to_image(input_text, output_image_path, contrast_factor=1.5):
     # Set the image size based on the text length and font size
     font_size = 14
     lines = input_text.strip().split('\n')
@@ -100,17 +104,44 @@ def text_to_image(input_text, output_image_path):
     # Draw the text on the image
     y_position = 0
     for line in lines:
-        draw.text((0, y_position), line, font=font, fill='black')
+        draw.text((0, y_position), line, font=font, fill='blue')
         y_position += font_size
 
+    enhancer = ImageEnhance.Contrast(image)
+    image_with_contrast = enhancer.enhance(contrast_factor)
     # Save the image
-    image.save(output_image_path)
+    image_with_contrast.save(output_image_path)
 
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     # Call the wait_for_photo function
     wait_for_photo(message)
+
+
+@bot.message_handler(commands=["text"])
+def wait_text(message):
+    user_id = message.from_user.id
+    bot.send_message(user_id, '```\nprint("Hello! Send me a text")\n```',
+                     parse_mode='MarkdownV2')
+    response = '```\n# hint\n```' + '```\nprint("keep it short")\n```'
+    bot.send_message(user_id, response,
+                     parse_mode='MarkdownV2')
+    bot.register_next_step_handler(message, send_text)
+
+
+def send_text(message):
+    user_id = message.from_user.id
+    user_input = message.text.strip().lower()
+    ascii_art = text2art(user_input)
+
+    output_image_path_3 = f"{media_folder}/text_output_{random_filename}.png"
+    text_to_image(ascii_art, output_image_path_3, contrast_factor=1.5)
+    image = output_image_path_3
+    file = open('./' + image, 'rb')
+    bot.send_photo(message.chat.id, file)
+    bot.send_message(user_id, '```\nprint("done!")\n```',
+                     parse_mode='MarkdownV2')
 
 
 bot.infinity_polling(timeout=10, long_polling_timeout=5)
